451031003dd73fbade5e109f1055b82d5f8af511
AlexandraAlter <alexandra.alter@protonmail.com>
2021-05-08 16:46:46 +0100

    Added proper support for X

--- plover_keyboardplus/machine.py
+++ plover_keyboardplus/machine.py
@@ -1,14 +1,29 @@
 from plover import _
 from plover.machine.keyboard import Keyboard
-from plover.oslayer.keyboardcontrol import KeyboardCapture
+
+from plover_keyboardplus.keyboardcontrol import KeyboardPlusCapture
 
 
 # i18n: Machine name.
 _._('KeyboardPlus')
 
-EXTRA_F_KEYS = 'F13 F14 F15 F16 F17 F18 F19 F20 F21 F22 F23 F23'
-
 
 class KeyboardPlus(Keyboard):
-    KEYS_LAYOUT = KeyboardCapture.SUPPORTED_KEYS_LAYOUT.replace('F12', 'F12 ' + EXTRA_F_KEYS)
+    KEYS_LAYOUT = KeyboardPlusCapture.SUPPORTED_KEYS_LAYOUT
+    ACTIONS = ('arpeggiate',)
+    
+    def __init__(self, params):
+        super().__init__(params)
 
+    def start_capture(self):
+        self._initializing()
+        try:
+            self._keyboard_capture = KeyboardPlusCapture()
+            self._keyboard_capture.key_down = self._key_down
+            self._keyboard_capture.key_up = self._key_up
+            self._suppress()
+            self._keyboard_capture.start()
+        except:
+            self._error()
+            raise
+        self._ready()
